813295b4d2925c61e36eb9d78bbcd2f78248bef7
mingkiphotography-dotcom <mingki.photography@gmail.com>
2026-06-17 18:41:44 +0800

    feat: bump version badge to v1.3.2 to indicate API key conflict fix

--- src/App.jsx
+++ src/App.jsx
@@ -366,7 +366,7 @@ function App() {
         </div>
         <div className="header-badge">
           <Sparkles size={12} />
-          <span>v1.3.1</span>
+          <span>v1.3.2</span>
         </div>
       </header>
 
